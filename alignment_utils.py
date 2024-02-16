0fa54dfd6c0bc8f65d1c82b195e462facce7d252
JonWinkelman <jon.winkelman@gmail.com>
2024-02-16 16:38:06 -0600

    sped up functions

--- alignment_utils.py
+++ alignment_utils.py
@@ -1,52 +1,55 @@
 from jw_utils import parse_fasta as pfa
+import pandas as pd
 
 
-def delete_alignment_positions(seq_dict, positions_to_delete):
-    """delete position in alignment, 0-based and return alignment dict
-    
-    parameters:
-    seq_dict (dict): {seq_id:aligned sequence}
-    positions_to_delete (list or other iterable): positions/columns (zero-based) in the alignment to delete
-    """
-
-    seq_dict_edit = {}
-    for name, seq in seq_dict.items():
-        seq_edit = ''
-        for i, val in enumerate(seq):
-            if i not in positions_to_delete:
-                seq_edit += val
-        
-        seq_dict_edit[name] = seq_edit
-    return seq_dict_edit
-
-
-
-def get_value_frequency(seq_dict, value_of_interest='-'):
-    """Return dict of columns and fraction of a values occurrence in that column
+def get_frequency_dict(aln_dict):
+    """retrun a dict with the frequency of each character in each column of alignment
     
     parameters
-    seq_dict (dict) {seq_id:aligned sequence}
-    value_of_interest (str): value of interest to return frequency for, e.g. for gaps enter '-',
+    aln_dict : dict
+        dictionary with sequence id as key and aligned sequence as value
 
-    return:
-    frequency dict, {
+    return
+        nested dictionary with main key as aln column,and nested d as
+        with character as key and frequency as value
+        
+        e.g. for the zero-ith column in alignment:
+        d[0] = {'A': 0.25,  'C': 0,  'G': 0.5.,  'T': 0.25,  'N': 0, '-': 0}
     """
-    num_seqs = len(seq_dict)
-    aln_length = len(list(seq_dict.values())[0])
-    freq_dict = {}
-    cols = {i:[] for i in range(aln_length)}
-    for name, aln_seq in seq_dict.items():
-        for i, col in enumerate(aln_seq):
-            cols[i].append(col)
-    i=0
-    for column, values in cols.items():
-        fraction = len([val for val in values if val == value_of_interest])/num_seqs
-        freq_dict[i] = fraction
-        i+=1
-    return freq_dict
+    
+    df = pd.DataFrame.from_dict(aln_dict, orient='index', columns=['sequence'])
+    df = df['sequence'].apply(list).apply(pd.Series)
+    num_sequences = df.shape[0]
+    
+    def make_blank_char_count_dict():
+        return {
+            'A': 0,  # Adenine
+            'C': 0,  # Cytosine
+            'G': 0,  # Guanine
+            'T': 0,  # Thymine
+            'N': 0,  # Any base (A or C or G or T)
+            '-': 0,  # gap in alignment
+        }
+        
+    char_counts_ineach_column = {}    
+    for i,col in enumerate(df):
+        character_counts = make_blank_char_count_dict()
+        col_lst = list(df[col])
+        unique_chars = set(col_lst)
+        if len(unique_chars) == 1:
+            character_counts[ list(unique_chars)[0] ] = 1
+            char_counts_ineach_column[i] = character_counts
+        else:
+            d = df[col].value_counts().to_dict()
+            for base, count in d.items():
+                freq = count/num_sequences
+                character_counts[base] = freq
+                char_counts_ineach_column[i] = character_counts
+    return char_counts_ineach_column
+    
 
 
-def positions_to_delete(aln_seq_dict, threshold, value_of_interest='-'):
+def positions_to_delete(aln_dict, threshold=1, character_of_interest='-'):
     """"Return a list of columns (0-based) in alignment that are above threshold.
 
     parameters
@@ -60,18 +63,22 @@ def positions_to_delete(aln_seq_dict, threshold, value_of_interest='-'):
         exceeds the threshold
     
     """
-    positions_to_delete = []
-    freq_dict = get_value_frequency(aln_seq_dict, value_of_interest=value_of_interest)
-    for pos, freq in freq_dict.items():
-        if freq > threshold:
-            positions_to_delete.append(pos) 
-    return positions_to_delete
+    columns_to_delete = []
+    columns_freq_dict = get_frequency_dict(aln_dict)
+    
+    for column, freq_d in columns_freq_dict.items():
+        for char, freq in freq_d.items():
+            if char == character_of_interest:
+                if freq >= threshold:
+                    columns_to_delete.append(column) 
+    return columns_to_delete
 
 
-def delete_alignment_positions(aln_seq_dict, threshold=0.8, value_of_interest='-' ):
-    """delete position in alignment, 0-based
-
-    aln_seq_dict (dict):
+def delete_alignment_positions(aln_dict, threshold=0.8, character_of_interest='-' ):
+    """delete 0-based position in alignment if character_of_interest's frequency in column is >= threshold.
+     **useful for removing columns that contain all or mostly gaps, or removing positions that 
+     do not have phylogenetic information
+    aln_dict (dict):
     threshold (int): if the frequency of value of interest is above the threshold value, 
             then that alignment position is added to list
     value_of_interest (str): value of interest to obtain frequency for in columns. e.g. to delete columns
@@ -82,13 +89,9 @@ def delete_alignment_positions(aln_seq_dict, threshold=0.8, value_of_interest='-
     edited sequence alignment dictionary with offending columns removed.
     """
 
-    positions = positions_to_delete(aln_seq_dict, threshold=threshold, value_of_interest='-')
-    seq_dict_edit = {}
-    for name, seq in aln_seq_dict.items():
-        seq_edit = ''
-        for i, val in enumerate(seq):
-            if i not in positions:
-                seq_edit += val
-        
-        seq_dict_edit[name] = seq_edit
+    positions = positions_to_delete(aln_dict, threshold=threshold, character_of_interest=character_of_interest)
+    df = pd.DataFrame.from_dict(aln_dict, orient='index', columns=['sequence'])
+    df = df['sequence'].apply(list).apply(pd.Series)
+    df = df.drop(columns=positions)
+    seq_dict_edit = df.apply(lambda row: ''.join(row), axis=1).to_dict()
     return seq_dict_edit
