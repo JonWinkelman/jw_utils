14fd931c0a274a2e0672a5982d7f1b28efde2460
JonWinkelman <jon.winkelman@gmail.com>
2024-08-09 15:30:07 -0400

    minor

--- useful_functions.py
+++ useful_functions.py
@@ -1,5 +1,6 @@
 import random
 from time import sleep
+import pandas as pd
 
 def move_cursor(mins):
     "move cursor in random dir once every min for given minutes"
@@ -8,3 +9,8 @@ def move_cursor(mins):
     for i in range(mins):
         pyautogui.moveTo(random.randint(1,500), random.randint(1,500), duration = 1)
         sleep(60)
+
+def save_df_to_doc(df):
+    fp = '/Users/jonathanwinkelman/Documents/temp.csv'
+    df.to_csv(fp)
+    return fp
