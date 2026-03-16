d4d9b20c1108319301959b2a97033d473c6c823c
Jonathan <jon.winkelman@gmail.com>
2026-03-16 13:33:35 -0500

    add matplotlib settings module

---
+++ mpl_settings.py
@@ -0,0 +1,36 @@
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+
+
+
+
+
+
+def update_settings(): 
+    mpl.rcParams['svg.fonttype'] = 'none'
+    plt.rcParams.update({
+        "font.size": 8,
+        "axes.linewidth": 0.8,
+        "axes.labelsize": 8,
+        "axes.titlesize": 8,
+        "xtick.labelsize": 7,
+        "ytick.labelsize": 7,
+        "xtick.major.width": 0.8,
+        "ytick.major.width": 0.8,
+        "xtick.major.size": 3,
+        "ytick.major.size": 3,
+        "lines.linewidth": 1,
+        "figure.dpi": 300,
+        'svg.fonttype':'none'
+    })
+
+def format_subplots(axes):
+    for ax in axes.flatten():
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.tick_params(direction='out', length=3, width=0.8)
+
+def format_ax(ax):   
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.tick_params(direction='out', length=3, width=0.8)
